e7d5ecd1f0ec94473a57b6456fef1b7dcb530885
Ahmed Sobh <sobh7771@gmail.com>
2024-10-17 08:51:32 +0300

    Remove unnecessary files to clean up the project structure

--- app/[locale]/jobs/types.ts
+++
@@ -1,8 +0,0 @@
-import { JobListing, User } from '@/lib/drizzle/schema';
-
-export type JobListingWithUser = Partial<JobListing> & {
-  user: Partial<User> & { profilePic: string }; // Linked user who posted the job
-};
-
-// TODO: Remove the use of Partial when the full JobListing and User models are ready
-// TODO: ProfilePic is a temporary addition, to be replaced when User schema is finalized
